8386503fb5f7a8daa14d90c9dca9b9d52d903813
Nathan C <32592701+awakedev@users.noreply.github.com>
2019-04-03 16:48:02 +0100

    Initial commit

---
+++ nn-restauraunt.js
@@ -0,0 +1,54 @@
+
+const brain = require ('brain.js');
+const restaurants = {
+    "Brilliant Yellow Corral": "Monday",
+    "Penny’s": "Tuesday",
+    "Right Coast Wings": "Wednesday",
+    "The Delusion Last Railway Car": "Thursday",
+    "Fun Day Inn": "Friday",
+    "JHOP": "Saturday",
+    "Owls": "Sunday"
+};
+
+// input : { Monday, Tuesday, etc. }
+// output : { Rest1, Rest2, etc. }
+
+const trainingData = [];
+
+for (let restaurantName in restaurants) {
+    const dayOfWeek = restaurants[restaurantName];
+    trainingData.push({
+        input: {[dayOfWeek]: 1 },
+        output: { [restaurantName]: 1}
+    });
+}
+
+const net = new brain.NeuralNetwork({ hiddenLayers: [3] });
+
+const stats = net.train(trainingData);
+
+console.log(stats);
+
+console.log(net.run({ 'Monday': 1 }));
+
+function restaurantForDay(dayOfWeek) {
+    const result = net.run({ [dayOfWeek]: 1 });
+    let highestValue = 0;
+    let highestRestaurantName = '';
+    for (let restuarantName in result) {
+        if (result[restuarantName] > highestValue) {
+            highestValue = result[restuarantName];
+            highestRestaurantName = restuarantName;
+        }
+    }
+    
+    return highestRestaurantName;
+}
+
+console.log(restaurantForDay('Monday'));
+console.log(restaurantForDay('Monday'));
+console.log(restaurantForDay('Monday'));
+console.log(restaurantForDay('Monday'));
+console.log(restaurantForDay('Monday'));
+console.log(restaurantForDay('Monday'));
+console.log(restaurantForDay('Monday'));
